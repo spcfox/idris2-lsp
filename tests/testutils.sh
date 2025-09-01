81be576782f717e41695ff95d49e1e8e120d9d80
Viktor Yudov <me@spcfox.com>
2025-09-01 22:00:56 +0300

    [ test ] Add test for open file

--- tests/testutils.sh
+++ tests/testutils.sh
@@ -5,6 +5,7 @@
 # Usage: . ../../testutils.sh
 
 idris2_lsp="$1"
+WORKING_DIR=$(pwd)
 
 # Delete build files between runs to prevent unexpected differences.
 # As this is at the top-level, this is run when this script is imported.
@@ -14,15 +15,36 @@ format_request() {
     printf "Content-Length: %d\r\n\r\n%s" "$(printf "%s" "$request" | wc -c)" "$1"
 }
 
+substitute_vars() {
+    file="$1"
+    input="$2"
+    sed \
+        -e "s|{WORKING_DIR}|$WORKING_DIR|g" \
+        -e "s|{FILE}|$file|g" \
+        "$input"
+}
+
 format_jsonl() {
-    input="$1"
-    while IFS= read -r line; do
+    file="$1"
+    input="$2"
+    substitute_vars "$file" "$input" | while IFS= read -r line; do
         request=$(printf "%s" "$line" | tr -d ' \t\n\r')
         [ -z "$request" ] && continue
         format_request "$request"
-    done <"$input"
+    done
 }
 
+clean_paths() {
+    sed \
+        -e "s|$WORKING_DIR|{WORKING_DIR}|g" \
+        -e "s|Content-Length: [0-9]\{1,\}|Content-Length: N|g"
+}
+
+run_lsp_no_clean() {
+    format_jsonl "$1" "$2" | $idris2_lsp
+}
+
+# Usage: run_lsp <file> <input>
 run_lsp() {
-    format_jsonl "$1" | $idris2_lsp
+    run_lsp_no_clean "$1" "$2" | clean_paths
 }
